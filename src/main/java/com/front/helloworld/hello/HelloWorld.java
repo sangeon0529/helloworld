db09e5fc1ba302b3cfd606e6cb614699d219c351
BIT <BIT@DESKTOP-QOHTAOV>
2022-01-12 10:25:50 +0900

    asd

--- src/main/java/com/front/helloworld/hello/HelloWorld.java
+++ src/main/java/com/front/helloworld/hello/HelloWorld.java
@@ -1,10 +1,20 @@
 package com.front.helloworld.hello;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HelloWorld {
 
+	public static int count = 10;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		hello();
 		System.out.println("hello world!!!!!!!!");
 	}
 
+	public static void hello() {
+		Map<String, Object> m = new HashMap<>();
+		
+	}
 }
